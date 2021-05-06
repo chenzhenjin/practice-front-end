42864374c5ab27d5d5edb64ff0bdc77f8e3d6fa1
chenzhenjin <971561770@qq.com>
2021-05-06 17:23:01 +0800

    feat(promise resolve): 深入理解then返回promise源码操作 帮助理解异步并发数限制

--- js/promise.js
+++ js/promise.js
@@ -1,93 +1,129 @@
-var pending = 'pending'
-var fullfilled = 'fullfilled'
-var rejected = 'rejected'
+//函数懒执行 setTimeout模拟微任务
+var pending = "pending";
+var fullfilled = "fullfilled";
+var rejected = "rejected";
 
 function myPromise(execute) {
-  var that = this
-  that.status = pending
-  that.onfullfillArray = []
-  that.onrejectArray = []
+  var that = this;
+  that.status = pending;
+  that.onfullfillArray = [];
+  that.onrejectArray = [];
   function resolve(value) {
     if (that.status === pending) {
       setTimeout(function () {
-        that.status = fullfilled
-        that.value = value
+        that.status = fullfilled;
+        that.value = value;
         that.onfullfillArray.forEach(function (fn) {
-          fn(value)
-        })
-      })
+          fn(value);
+        });
+      });
     }
   }
   function reject(reason) {
     setTimeout(function () {
       if (that.status === pending) {
-        that.status = rejected
-        that.reason = reason
+        that.status = rejected;
+        that.reason = reason;
         that.onrejectArray.forEach(function (fn) {
-          fn(reason)
-        })
+          fn(reason);
+        });
       }
-    })
+    });
   }
   try {
-    execute(resolve, reject)
+    execute(resolve, reject);
   } catch (err) {
-    reject(err)
+    reject(err);
   }
 }
 
 myPromise.prototype.then = function (onfullfillfn, onrejectfn) {
-  var that = this
-  var promise
-  onfullfillfn = typeof onfullfillfn === 'function' ? onfullfillfn : function (value) { return value }
-  onrejectfn = typeof onrejectfn === 'function' ? onrejectfn : function (reason) { throw reason }
+  var that = this;
+  var promise;
+  onfullfillfn =
+    typeof onfullfillfn === "function"
+      ? onfullfillfn
+      : function (value) {
+          return value;
+        };
+  onrejectfn =
+    typeof onrejectfn === "function"
+      ? onrejectfn
+      : function (reason) {
+          throw reason;
+        };
   if (that.status === fullfilled) {
     promise = new myPromise(function (resolve, reject) {
       setTimeout(function () {
         try {
-          onfullfillfn(that.value)
+          let x = onfullfillfn(that.value);
+          resolve(x);
         } catch (err) {
-          reject(err)
+          reject(err);
         }
-      })
-    })
+      });
+    });
   }
   if (that.status === rejected) {
-    promise = new Promise(function (resolve, reject) {
+    promise = new myPromise(function (resolve, reject) {
       setTimeout(function () {
         try {
-          onrejectfn(that.reason)
+          let x = onrejectfn(that.reason);
+          reject(x);
         } catch (err) {
-          reject(err)
+          reject(err);
         }
-      })
-    })
+      });
+    });
   }
   if (that.status === pending) {
-    promise = new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        that.onfullfillArray.push(function () {
-          try {
-            let x = onfullfillfn(that.value)
-            resolve(x)
-            // console.log('then pending', x)
-            // resolvePromise(promise, x, resolve, reject)
-          } catch (err) {
-            reject(err)
+    promise = new myPromise(function (resolve, reject) {
+      that.onfullfillArray.push(function () {
+        try {
+          let x = onfullfillfn(that.value);
+          if (x instanceof myPromise) {
+            //处理then返回promise处理，继续对pending状态进行then，因为返回promise，再异步开启
+            console.log("instanceof");
+            if (x.status === pending) {
+              //根据userpromise状态为等待，新增then方法设置回调函数
+              //转移userpromise resolve执行userpromise then回调后到
+              //接收userpromise resolve的值，传递到thenpromise._resolve,更新thenpromise状态、值和执行用户定义then回调
+              //_resolve为了解决thenpromise已经resolve，要再次执行then回调
+              x.then((value) => {
+                promise._resolve(value);
+              });
+            }
+          } else {
+            resolve(x); //x为promise 相当于 resolve(new promise())
           }
-        })
-        that.onrejectArray.push(function () {
-          try {
-            onrejectfn(that.reason)
-          } catch (err) {
-            reject(err)
-          }
-        })
-      })
-    })
+          // console.log('then pending', x)
+          // resolvePromise(promise, x, resolve, reject)
+        } catch (err) {
+          reject(err);
+        }
+      });
+      that.onrejectArray.push(function () {
+        try {
+          let x = onrejectfn(that.reason);
+          reject(x);
+          onrejectfn(that.reason);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
   }
-  return promise
-}
+  return promise;
+};
+
+myPromise.prototype._resolve = function (value) {
+  if (this.status !== pending) return;
+  setTimeout(() => {
+    this.status = fullfilled;
+    this.value = value;
+    this.onfullfillArray.forEach((cb) => cb(this.value));
+  }, 0);
+};
 
 // function resolvePromise(promise, x, resolve, reject) {
 //   if (typeof x === 'function') {
@@ -106,18 +142,20 @@ myPromise.prototype.then = function (onfullfillfn, onrejectfn) {
 //     resolve(x)
 //   }
 // }
-
+// 实例化执行callback 进入异步宏任务跳出。继续执行then，收集onfullfiedfn，等待宏任务完成触发执行onfullfiedfn
 var test = new myPromise(function (resolve, reject) {
   setTimeout(function () {
-    resolve(10)
-  }, 500)
-}).then(function (res) {
-  console.log('res1', res)
-  return new myPromise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve(5)
-    }, 500)
-  })
-}).then(function (res) {
-  console.log('res2', res)
+    resolve(10);
+  }, 500);
 })
+  .then(function (res) {
+    console.log("res1", res);
+    return new myPromise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve(5);
+      }, 500);
+    });
+  }) //因为返回的promise的状态为pendding，这个then还是会等待返回的promise的resolve触发
+  .then(function (res) {
+    console.log("res2", res);
+  });
---
+++ js/resolve.js
@@ -0,0 +1,99 @@
+/*
+ * @Author: chenzhenjin
+ * @Email: BrotherStudy@163.com
+ * @Date: 2021-05-06 10:56:34
+ * @LastEditTime: 2021-05-06 15:25:51
+ * @Descripttion: resolve方法
+ */
+function callback() {
+  console.log("callback");
+}
+// let promise2 = new Promise(function (resolve, reject) {
+//   console.log('Promise')
+//   setTimeout(() => {
+//     resolve(6);
+//   });
+// }).then(() => callback());
+let promise1 = new Promise((resolve, reject) => {
+  console.log("promise1");
+  setTimeout(() => {
+    resolve(); //微任务触发
+  }); //宏任务触发
+}).then(() => {
+  console.log("promise1 then");
+});
+let promise2 = Promise.resolve().then(() => callback()); //等script宏任务完，就微任务触发执行then的onfullfiedfn**继续触发promise2.then**
+promise2.then(() => { 
+  //连续then情况下，默认Promise2.resolve().then，
+  //但是前个then回调返回了promise，就会继续等待这个promise执行resolve实参给promise2的then回调形参
+  console.log("promise2");
+});
+console.log("script end");
+//宏任务记录 script 最先执行 追加微任务记录执行 Promise.resolve().then promise2.then 这里全部执行完，才执行下一个宏任务
+//追加宏任务记录 setTimeout 微任务记录 new Promise.then
+
+/**
+ * 异步并发数限制
+ * 1. new promise 一经创建，立即执行
+ * 2. 使用 Promise.resolve().then 可以把任务加到微任务队列，防止立即执行迭代方法
+ * 3. 微任务处理过程中，产生的新的微任务，会在同一事件循环内，追加到微任务队列里
+ * 4. 使用 race 在某个任务完成时，继续添加任务，保持任务按照最大并发数进行执行
+ * 5. 任务完成后，需要从 doingTasks 中移出
+ */
+function limit(count, array, iterateFunc) {
+  const tasks = [];
+  const doingTasks = [];
+  let i = 0;
+  const enqueue = () => {
+    /*scripctmacro [
+        taskMicro: [
+          Promise.resolve().then 微任务执行回调 setTimeout
+          task.then 等setTimeout执行resolve触发then的回调
+          Promise.resolve().then 微任务执行回调 enqueue
+          Promise.resolve().then 微任务执行回调 setTimeout  
+          task.then 等setTimeout执行resolve触发then的回调
+          Promise.race(doingTasks) 封装的promise包裹Promise.resolve(promise).then((race)=>resolve(race))
+          等任一用户promise执行resolve触发then的回调 race
+          Promise.race(doingTasks).then 等race触发resolve触发then的回调 enqueue 异步递归
+        ]
+        limitMicro: [
+          limit.then
+        ]
+    ] 
+      */
+    if (i === array.length) {
+      return Promise.resolve();
+    }
+    const task = Promise.resolve().then(() => iterateFunc(array[i++]));
+    tasks.push(task);
+    console.log('tasks', tasks)
+    const doing = task.then(() => { //并不会马上执行，因为前个then回调返回了promise
+      console.log('doing', i)
+      doingTasks.splice(doingTasks.indexOf(doing), 1)
+    });
+    doingTasks.push(doing);
+    console.log('doingTasks', doingTasks)
+    const res =
+      doingTasks.length >= count ? Promise.race(doingTasks) : Promise.resolve();
+    return res.then(enqueue);
+  };
+  //设计很巧妙，就是tasks的变化，会影响到Promise.all判断，这样保证用户定义then是所有的task执行完
+  //类似于race，每个promise触发完resolve得到结果后，记录完成count++，
+  //当count满足promise个数，执行自身定义promise的resolve(),触发用户定义的then回调
+  return enqueue().then(() => {
+    console.log('all')
+    return Promise.all(tasks)
+  });
+}
+
+// test
+const timeout = (await) =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log('await', await);
+      resolve(await);
+    }, await)
+  );
+limit(2, [1000, 1000, 1000, 1000], timeout).then((res) => {
+  console.log('limit', res);
+});
